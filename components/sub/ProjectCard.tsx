5d0991cb54773d1bd87b87a09099313690b531a7
MrCyberlord <amanjain2691@gmail.com>
2024-02-15 00:29:17 +0530

    Tailwind Edit-10

--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -36,15 +36,15 @@ const ProjectCard = ({ src, title, description, techStack, appLink, gitLink }: P
         />
         </div>
       </div >
-      <div className='p-[2px] px-2 h-[4.5rem] w-[24rem] sm:p-1 sm:w-[28rem] sm:h-[6.5rem]'>
+      <div className='p-[2px] px-2  w-[25rem] sm:p-1 sm:w-[28rem] sm:h-[6.5rem]'>
             <h1 className="text-[16px] text-center font-semibold text-blue-700 leading-[1]
             sm:text-xl sm:leading-tight">{title}</h1>
-            <p className='text-[13px] text-blue-300 leading-none pb-1
+            <p className='text-[14px] text-blue-300 leading-none pb-1
             sm:text-[16px] sm:leading-[1]'>{description}</p>
-            <p className='text-[13px] text-blue-500 leading-none
+            <p className='text-[14px] text-blue-500 leading-none
             sm:text-[16px] sm:leading-[1]'>{techStack}</p>
         </div>
-      <div className='flex w-full justify-evenly p-5'>
+      <div className='flex w-full justify-evenly p-1 sm:p-4'>
       <a href={appLink} target="_blank" rel="noopener noreferrer" className='projectbutton'>App Link</a>
       <a href={gitLink} target="_blank" rel="noopener noreferrer" className='projectbutton'>Git Link</a>
       </div>
